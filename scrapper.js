2bc319f5ce967ef80862ee8c2a478004853a824f
itheakmal <94047823+itheakmal@users.noreply.github.com>
2023-09-13 19:01:47 +0500

    first one

---
+++ scrapper.js
@@ -0,0 +1,124 @@
+const runScrapper = async (link) => {
+    return new Promise((resolve, reject) => {
+        const exec = require('child_process').exec
+        const fs = require('fs')
+        const moment = require('moment')
+        console.log('link', link)
+        try {
+            const jsonString = JSON.stringify({ link })
+            const path = __dirname
+            const fileName = `size_${moment().format('HHmmssSS')}.json`
+            const filePathName = path + fileName
+            fs.writeFileSync(`${fileName}`, jsonString)
+            const cmd = `python /size_scrapper/update_size.py ${fileName}`
+            exec(cmd, function (error, stdout, stderr) {
+                if (error) {
+                    reject(error)
+                    return
+                }
+                if (stdout) {
+                    resolve({ stdout, stderr })
+                }
+            })
+        } catch (error) {
+            reject(error)
+        }
+
+    })
+}
+const runOrderScrapper = async (address, scrapper) => {
+    return new Promise((resolve, reject) => {
+        try {
+            const exec = require('child_process').exec;
+            const fs = require('fs');
+        const moment = require('moment')
+
+            const jsonString = JSON.stringify(address, null, 2);
+            const path = __dirname;
+            const fileName = `order${moment().format('HHmmssSS')}.json`;
+            console.log('filename', fileName)
+            const filePathName = path + fileName;
+            fs.writeFileSync(`${fileName}`, jsonString);
+            // const cmd = `python3 /var/www/similar-products/banana_republic.py address=${address}`;
+            const cmd = `python /size_scrapper/${scrapper}.py ${fileName}`;
+
+            exec(cmd, function (error, stdout, stderr) {
+                if (error) {
+                    // fs.unlinkSync(filePathName);
+                    reject(error);
+                    return;
+                }
+
+                if (stdout) {
+                    // fs.unlinkSync(filePathName);
+                    resolve({ stdout, stderr });
+                }
+            });
+        } catch (error) {
+            reject(error)
+        }
+
+    })
+}
+
+const readFile = async (file) => {
+    const fs = require('fs').promises
+    const filed = file.trim()
+    console.log('file', filed)
+    try {
+        const data = await fs.readFile(`./${filed}`, 'utf-8')
+        return JSON.parse(data)
+    } catch (error) {
+        throw error
+    }
+}
+const readOrderFile = async (file) => {
+    const fs = require('fs').promises;
+    // const filed = file.replace(/\r\n/g, '');
+    const filed = file.trim().split(',');
+
+
+    try {
+        // console.log('sails.config.local', sails.config);
+        const data = []
+        for (let file of filed) {
+
+            const temp = await fs.readFile(`./${file}`, 'utf-8');
+            if (temp !== null) {
+                data.push(JSON.parse(temp))
+            } else {
+                data.push(temp)
+
+            }
+        }
+        // console.log('User service data', data);
+        return data;
+    } catch (err) {
+        throw err;
+    }
+}
+
+exports.sizeScrapper = (link) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const { stdout, stderr } = await runScrapper(link)
+            const size = await readFile(stdout)
+            resolve(size)
+        } catch (error) {
+            reject(error)
+        }
+    })
+}
+exports.orderScrapper = (data, file) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const { stdout, stderr } = await runOrderScrapper(data, file)
+            
+            const order = await readOrderFile(stdout)
+            console.log('order', JSON.stringify(order))
+            resolve(order)
+        } catch (error) {
+            reject(error)
+        }
+    })
+}
---
+++ server.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var app = express();
+const cors = require('cors')
+const {sizeScrapper, orderScrapper} = require('./scrapper')
+// const corsOptions = {
+//    origin: *
+//    // origin: '165.232.147.215'
+// }
+// app.use(cors(corsOptions))
+app.use(express.json())
+app.get('/', function (req, res) {
+   res.send('Hello World');
+})
+app.post('/size-scrapper', async (req, res) => {
+   const data = req.body
+   console.log('data', data)
+   try {
+      
+      const result = await sizeScrapper(data.link)
+      res.json(result);
+   } catch (error) {
+      console.log(error)
+   }
+})
+app.post('/order-scrapper', async (req, res) => {
+   const data = req.body
+   console.log('data', data)
+   try {
+      
+      const result = await orderScrapper(data.cartItem, data.scrapper)
+      console.log('result', result)
+      res.json(result);
+   } catch (error) {
+      console.log(error)
+   }
+})
+
+var server = app.listen(8989, function () {
+   var host = server.address().address
+   var port = server.address().port
+   
+   console.log("atricent scrapper listening at http://%s:%s", host, port)
+})
+
