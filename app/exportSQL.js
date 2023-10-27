ce2ff09847b5c7d86a2c94a9032fcecce28b91f6
itheakmal <94047823+itheakmal@users.noreply.github.com>
2023-10-27 16:30:11 +0500

    update

--- app/exportSQL.js
+++ app/exportSQL.js
@@ -27,22 +27,53 @@ exports.exportSQL = async (filePath, fileName) => {
             password: '',
             database: 'atricent_live',
         },
-        dump: { 
-            schema: { table: { dropIfExist: true } } ,
-            data: {maxRowsPerInsertStatement: 10,},
+        dump: {
+            schema: { table: { dropIfExist: true } },
+            data: { maxRowsPerInsertStatement: 10, },
             tables: ['sizes', 'brands'],
         },
         // dumpToFile: 'app/data/size_dump.sql.gz',
         // dumpToFile: 'app/data/size_dump.sql',
-        dumpToFile: filePath+fileName,
+        dumpToFile: filePath + fileName,
         // compressFile: true,
-    }).then((data)=>{
+    }).then((data) => {
         console.log('DB exported')
         // sql2gzip(filePath, fileName)
-        importSQL(filePath, fileName)
+        // importSQL(filePath, fileName)
+        const parser = require('node-sql-parser');
+        const fs = require('fs');
+
+        // Read the SQL file
+        const sql = fs.readFileSync(filePath + fileName, 'utf8');
+
+        // Parse the SQL file
+        const ast = parser.parse(sql);
+
+        // Modify the table names in the AST
+        ast.stmt.forEach((stmt) => {
+            if (stmt.table.table === 'sizes') {
+                stmt.table.table = 'sizes_1';
+            } else if (stmt.table.table === 'variations') {
+                stmt.table.table = 'variations_1';
+            }
+        });
+
+        // Generate the modified SQL
+        const modifiedSql = parser.stringify(ast);
+        const newFileName = fileName.split('.')[0] + '_1.sql'
+        console.log('newFileName')
+        // Write the modified SQL to a new file
+        fs.writeFileSync(filePath + newFileName, modifiedSql);
+        console.log('WriteFileSync is done!');
+
+
+
+
+
+
     })
     console.log('consoled')
-    
+
     // }
 
 
