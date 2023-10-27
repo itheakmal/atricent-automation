c61f54a7f8e9d84b32b72fb3a87b9d180f38fcbc
itheakmal <94047823+itheakmal@users.noreply.github.com>
2023-10-27 17:14:13 +0500

    Update exportSQL.js

--- app/exportSQL.js
+++ app/exportSQL.js
@@ -18,61 +18,80 @@ exports.exportSQL = async (filePath, fileName) => {
     // step 1,2,3
     // dump the result straight to a compressed file
     // const dumpDB = () => {
-// mysql -u doadmin -pAVNS_o9klOLdLGD5avJq3GMt -h dbaas-db-1815241-do-user-7338207-0.b.db.ondigitalocean.com -P 25060 atricent_scrappers
+    // mysql -u doadmin -pAVNS_o9klOLdLGD5avJq3GMt -h dbaas-db-1815241-do-user-7338207-0.b.db.ondigitalocean.com -P 25060 atricent_scrappers
     console.log('file started')
-    mysqldump({
-        connection: {
-            host: 'dbaas-db-1815241-do-user-7338207-0.b.db.ondigitalocean.com',
-            port: 25060,
-            user: 'doadmin',
-            password: 'AVNS_o9klOLdLGD5avJq3GMt',
-            database: 'live_atricent',
-        },
-        dump: {
-            schema: { table: { dropIfExist: true } },
-            data: { maxRowsPerInsertStatement: 10, },
-            tables: ['sizes', 'variations'],
-        },
-        // dumpToFile: 'app/data/size_dump.sql.gz',
-        // dumpToFile: 'app/data/size_dump.sql',
-        dumpToFile: filePath + fileName,
-        // compressFile: true,
-    }).then((data) => {
-        console.log('DB exported')
-        // sql2gzip(filePath, fileName)
-        // importSQL(filePath, fileName)
-        const parser = require('node-sql-parser');
-        const fs = require('fs');
+    // mysqldump({
+    //     connection: {
+    //         host: 'dbaas-db-1815241-do-user-7338207-0.b.db.ondigitalocean.com',
+    //         port: 25060,
+    //         user: 'doadmin',
+    //         password: 'AVNS_o9klOLdLGD5avJq3GMt',
+    //         database: 'live_atricent',
+    //     },
+    //     dump: {
+    //         schema: { table: { dropIfExist: true } },
+    //         data: { maxRowsPerInsertStatement: 10, },
+    //         tables: ['sizes', 'variations'],
+    //     },
+    //     // dumpToFile: 'app/data/size_dump.sql.gz',
+    //     // dumpToFile: 'app/data/size_dump.sql',
+    //     dumpToFile: filePath + fileName,
+    //     // compressFile: true,
+    // }).then((data) => {
+    //     console.log('DB exported')
+    //     // sql2gzip(filePath, fileName)
+    //     // importSQL(filePath, fileName)
+    //     const parser = require('node-sql-parser');
+    //     const fs = require('fs');
 
-        // Read the SQL file
-        const sql = fs.readFileSync(filePath + fileName, 'utf8');
+    //     // Read the SQL file
+    //     const sql = fs.readFileSync(filePath + fileName, 'utf8');
 
-        // Parse the SQL file
-        const ast = parser.parse(sql);
+    //     // Parse the SQL file
+    //     const ast = parser.parse(sql);
 
-        // Modify the table names in the AST
-        ast.stmt.forEach((stmt) => {
-            if (stmt.table.table === 'sizes') {
-                stmt.table.table = 'sizes_1';
-            } else if (stmt.table.table === 'variations') {
-                stmt.table.table = 'variations_1';
-            }
-        });
+    //     // Modify the table names in the AST
+    //     ast.stmt.forEach((stmt) => {
+    //         if (stmt.table.table === 'sizes') {
+    //             stmt.table.table = 'sizes_1';
+    //         } else if (stmt.table.table === 'variations') {
+    //             stmt.table.table = 'variations_1';
+    //         }
+    //     });
 
-        // Generate the modified SQL
-        const modifiedSql = parser.stringify(ast);
-        const newFileName = fileName.split('.')[0] + '_1.sql'
-        console.log('newFileName')
-        // Write the modified SQL to a new file
-        fs.writeFileSync(filePath + newFileName, modifiedSql);
-        console.log('WriteFileSync is done!');
-
-
+    //     // Generate the modified SQL
+    //     const modifiedSql = parser.stringify(ast);
+    //     const newFileName = fileName.split('.')[0] + '_1.sql'
+    //     console.log('newFileName')
+    //     // Write the modified SQL to a new file
+    //     fs.writeFileSync(filePath + newFileName, modifiedSql);
+    //     console.log('WriteFileSync is done!');
 
 
 
 
-    })
+
+
+    // })
+    const mysql = require('mysql2');
+
+    // create the connection to database
+    const connection = mysql.createConnection({
+        host: 'dbaas-db-1815241-do-user-7338207-0.b.db.ondigitalocean.com',
+        port: 25060,
+        user: 'doadmin',
+        password: 'AVNS_o9klOLdLGD5avJq3GMt',
+        database: 'live_atricent',
+    });
+
+    // simple query
+    connection.query(
+        'SELECT * FROM `user_auth`',
+        function (err, results, fields) {
+            console.log(results); // results contains rows returned by server
+            console.log(fields); // fields contains extra meta data about results, if available
+        }
+    );
     console.log('consoled')
 
     // }
