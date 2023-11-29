fe36475222545264362e20856bdb8eeee78f4218
itheakmal <94047823+itheakmal@users.noreply.github.com>
2023-11-29 16:27:55 +0500

    Update server.js

--- server.js
+++ server.js
@@ -68,24 +68,145 @@ app.post('/order-scrapper', async (req, res) => {
       console.log(error)
    }
 })
-app.post('/upload', async (req, res) => {
+app.post('/queue/size-scrapper', async (req, res) => {
 
    var fs = require('fs');
 
    try {
-      const fileStream = fs.createWriteStream('app/data/size_dump22.sql.gz');
-      req.on('data', (chunk) => {
-         fileStream.write(chunk);
-      });
+      let cartSizes = [];
+      let appResult = []
+      let index = 0;
+      for await (const cartItem of data.carts) {
+         let count = 0;
+         for (const varItem of cartItem.variations) {
+            let num = 0;
+            if (varItem?.link?.link) {
 
-      req.on('end', () => {
-         fileStream.end();
-         console.log('File uploaded successfully!');
-         res.writeHead(200, { 'Content-Type': 'text/plain' });
-         res.end('File uploaded successfully!');
+               const result = await sizeScrapper(varItem.link.link, io, varItem.id, num, count, index)
+               if (result) {
+                  for (let item of result) {
+                     item.item = varItem
+                     item.id = varItem.id
+                  }
+
+                  console.log('result', result)
+                  const parsedSize = result
+                  if (parsedSize.length) {
+                     let tempID = null
+                     const temp = parsedSize.map(ps => {
+                        if (tempID !== ps.id) {
+                           tempID = ps.id
+                        }
+                        const sample = ps.size_elements ? JSON.stringify(ps.size_elements) : ps.size_elements
+                        return { ...ps, size_elements: sample }
+                     })
+                     console.log('tempID before emitting ==>', tempID)
+                     console.log('temp before emitting ==>', temp)
+                     // appResult.push({id:tempID, data: temp})
+
+                     // appIO.socket.emit('sizeUpdate', {data: temp, id: tempID})
+                     // await Size.update({ variation: returnedItem.id }).set({ meta: temp })
+                     // await deleteGeneratedFile(stdout)
+
+                     const firstMatch = parsedSize.filter(size => {
+
+                        const tempType = size.type !== null ? size.type.toLowerCase() : size.type
+                        const tempLength = size.length !== null ? size.length.toLowerCase() : size.length
+
+                        const givenType = size.item.size[0] !== null ? size.item.size[0].toLowerCase() : size.item.size[0]
+                        const givenLength = size.item.size[1] !== null ? size.item.size[1].toLowerCase() : size.item.size[1]
+
+                        return tempType === givenType && tempLength === givenLength
+                     })
+
+                     if (firstMatch.length) {
+                        for (let item of firstMatch) {
+                           if (item.size_elements !== null) {
+
+                              console.log('item.item.size 1=======>', JSON.stringify(item.item.size))
+                              const ele = item.size_elements.findIndex(sizeItem => sizeItem.toLowerCase() == item.item.size[2].toLowerCase())
+                              console.log('ele', ele)
+                              if (ele === -1) {
+                                 const sizesReturned = {
+                                    id: item.item.id,
+                                    error: 'Size not available, wanna checkout other sizes'
+                                 }
+                                 cartSizes.push(sizesReturned)
+                              }
+                           } else {
+                              const sizesReturned = {
+                                 id: item.item.id,
+                                 error: 'All sizes are out of stock'
+                              }
+                              cartSizes.push(sizesReturned)
+                           }
+                        }
+                     } else {
+                        let id = 0
+                        for (let temp of parsedSize) {
+                           id = temp.id
+                           break
+                        }
+                        const sizesReturned = {
+                           id: id,
+                           error: 'No size available'
+                        }
+                        cartSizes.push(sizesReturned)
+                     }
+
+                  } else {
+                     console.log('no data returned from the scrapper')
+                  }
+                  // appIO.socket.emit('sizeScrapper', result);
+               } else {
+                  console.log('in else result', result)
+                  const temp = {}
+                  temp.response = result
+                  temp.id = varItem.id
+                  // appIO.socket.emit('sizeScrapper', [temp]);
+               }
+            } else {
+               console.log('in else result', result)
+               const temp = {}
+               temp.response = "Variation does not have link"
+               temp.id = varItem.id
+               cartSizes.push(temp)
+            }
+            num++
+         }
+         count++
+      }
+
+
+      // ---------------------------------- jugad
+
+
+
+
+
+
+      // }
+
+
+      console.log('cartSizes before emiting event-->', cartSizes)
+
+      // for (let cartItem of cartSizes) {
+      //    appResult[cartItem.id] = cartItem.error
+      // }
+      // console.log('appResult before emiting event-->', appResult)
+
+      // appIO.socket.emit('sizeUpdate', appResult)
+      appIO.socket.emit('sizeScrapperApp', cartSizes);
+      // sails.config.globals.appSocket.emit('sizeScrapperApp', { cartSizes });
+      cartSizes = [];
+      return res.status(200).json({
+         message: 'success'
       });
    } catch (error) {
-      console.log(error)
+      console.log(error);
+      return res.status(423).json({
+         message: 'failure'
+      })
    }
 })
 
