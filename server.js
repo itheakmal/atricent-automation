8e954f4629b919cd56d9e739ff444891d0128fea
itheakmal <94047823+itheakmal@users.noreply.github.com>
2023-12-21 15:01:58 +0500

    Update server.js

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ io.on('connection', socket => {
    appIO.socket.on('orderScrapper', async function (data) {
       console.log('data', data)
       const momentFileName = moment().format('HHmmssSS')
-      const asyncTask = runOrderScrapper(data.cartItem, data.scrapper, momentFileName)
+      const asyncTask = runOrderScrapper(data.cartItem.cartItem, data.cartItem.scrapper, momentFileName)
       try {
          const data = await asyncTask;
          console.log('Async task has completed', data);
